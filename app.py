34912834031013e1b907142538aa75da7d5d8455
Alberto Cañizares A <albertocanizares@MacBook-Pro-14-Alberto.local>
2025-11-05 13:43:19 +0100

    Rename proposal.html to index.html

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
-# Se ejecuta con streamlit run app.py
+# Se ejecuta con:
+# streamlit run app.py
 # Activar .venv con source .venv/bin/activate
 
 import streamlit as st
 from pathlib import Path
 
 st.set_page_config(
-    page_title="HR Prospecting & Matching - Proposal", layout="wide")
+    page_title="HR Prospecting & Matching - index", layout="wide")
 
 # Carga el HTML completo
-html_code = Path("proposal.html").read_text(encoding="utf-8")
+html_code = Path("index.html").read_text(encoding="utf-8")
 
 # Render del HTML
 st.components.v1.html(html_code, height=1400, scrolling=True)
